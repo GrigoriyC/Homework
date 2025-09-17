e4a10932c37566fb01ad4e9ef6860c1aeaaa04e8
GrigoriyS <smolencevgv@mail.ru>
2025-09-17 12:17:13 +0700

    DZ_na_20250916.

---
+++ DZ_na_20250916/task_4/main.py
@@ -0,0 +1,14 @@
+# Задание 4: Вычислите периметр и площадь прямоугольника
+# length = 15
+# width = 7
+# perimeter = ?
+# area = ?
+
+length = 15
+width = 7
+perimeter = 2 * (length + width)
+area = length * width
+
+print("Результаты подсчетов:")
+print("perimeter = ", perimeter)
+print("area = ", area)
